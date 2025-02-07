093d3cbc5ce60547bade7a7bd8ec979112769b56
TOM-SYY <iwork.chana@gmail.com>
2025-02-07 21:08:00 +0700

    Update dashboard_page.py

--- dashboard_page.py
+++ dashboard_page.py
@@ -201,6 +201,7 @@ if __name__ == '__main__':
     init_db()
   
     import os
-port = int(os.environ.get('PORT', 5000))  # ใช้ PORT จาก Render ถ้าไม่มีจะใช้ 5000
-socketio.run(app, host='0.0.0.0', port=10000)
+port = int(os.environ.get('PORT', 5000))  # ใช้ PORT จาก Render
+socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+
 
