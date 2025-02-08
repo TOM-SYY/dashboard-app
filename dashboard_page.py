057fcec3927fd66ffbf2da0fd0e6ff6e5f1562cb
TOM-SYY <iwork.chana@gmail.com>
2025-02-08 12:08:18 +0700

    Update dashboard_page.py

--- dashboard_page.py
+++ dashboard_page.py
@@ -198,5 +198,5 @@ def home():
     return redirect(url_for('login'))
 if __name__ == '__main__':
     init_db()
-socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+socketio.run(app, host='0.0.0.0', port=10000, allow_unsafe_werkzeug=True)
 
